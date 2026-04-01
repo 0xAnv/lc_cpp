4a2f990059926ddcf684125514f1081e0010a586
Anvesh Dange <anveshdange.ai@gmail.com>
2026-04-01 10:40:37 +0000

    optimised c++17

--- Basics/204_Count_Primes/CountPrimes.cpp
+++ Basics/204_Count_Primes/CountPrimes.cpp
@@ -36,7 +36,34 @@ public:
         // we add 1 at end to account for prime number 2
         return 1 + std::ranges::count_if(odd_numbers, isPrime);
     }
-    static int count_primes_optimised_cpp17(int n);
+    /*
+     * Approach 2: Sieve of Eratosthenes
+     *
+     */
+    static int count_primes_optimised_cpp17(int n)
+    {
+        if (n <= 2) return 0;
+        // c++ std::vector<bool> is space-optimised bitset; packs 8 booleans into single byte
+        // For n = 5,000,000 this takes ~610 kb, allowing entire array to fit in mordern cpu L2/L3 cache.
+        // Memory latency vanishes
+        std::vector<bool> isPrime(n, true);
+        // pre count initial primes, we assume 2 and all odd nums are prime
+        int count = n/2;
+        for (long long p = 3; p*p < n; p += 2)
+        {
+            if (isPrime[p])
+            {
+                for (long long i = p*p; i < n; i+= 2*p)
+                {
+                    if (isPrime[i])
+                    {
+                        isPrime[i] = false; count--;
+                    }
+                }
+            }
+        }
+        return count;
+    }
     static int count_primes_optimised_cpp20(int n);
 
 private:
@@ -61,6 +88,7 @@ int main()
     {
         std::cout<<"n: " <<x<<"; "<<"BruteforceCPP17: "<< Solution::count_primes_bruteforce_cpp17(x) << std::endl;
         std::cout<<"n: " <<x<<"; "<<"BruteforceCPP20: "<< Solution::count_primes_bruteforce_cpp20(x) << std::endl;
+        std::cout<<"n: " <<x<<"; "<<"OptimisedCPP17: "<< Solution::count_primes_optimised_cpp17(x) << std::endl;
         std::cout << "---------" << std::endl;
     });
 }
