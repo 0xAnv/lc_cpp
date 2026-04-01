a4fada147c69598a01878daa514cbeb6e0ca4bfa
Anvesh Dange <anveshdange.ai@gmail.com>
2026-04-01 09:15:45 +0000

    countPrime bruteforce approach

---
+++ Basics/204_Count_Primes/CountPrimes.cpp
@@ -0,0 +1,66 @@
+//
+// Created by anvesh on 4/1/26.
+//
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <ranges>
+
+class Solution
+{
+public:
+    /*
+     * Approach 1: Bruteforce
+     * We iterate through every number from 2 up to n-1;
+     * For each number, you check if it's prime.
+     * To check if a num is prim
+     */
+    static int count_primes_bruteforce_cpp17(int n)
+    {
+        if (n <= 2) return 0; // neg num, 0, 1
+        int count = 1; //starting at 1 since 2 is prime
+        for (int i = 3; i < n; i += 2)
+        {
+            if (isPrime(i)) ++ count;
+        }
+        return count;
+    }
+    static int count_primes_bruteforce_cpp20(int n)
+    {
+        if (n <= 2) return 0;
+        // creating view of numbers from 3 -> n-1, filtering to keep only odd number
+        auto odd_numbers = std::views::iota(3, n)
+                            | std::views::filter([](const int x){ return x%2 != 0;});
+        // count how many nums in our view satisfy isPrime condition
+        // we add 1 at end to account for prime number 2
+        return 1 + std::ranges::count_if(odd_numbers, isPrime);
+    }
+    static int count_primes_optimised_cpp17(int n);
+    static int count_primes_optimised_cpp20(int n);
+
+private:
+    // extremely optimised bruteforce primality test (6k +/- 1 rule)
+    static bool isPrime(const int n)
+    {
+        if (n <= 1) return false;
+        if (n <= 3) return true; // 2, 3 are primes
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        for (int i = 5; i*i <= n; i += 6) {if (n % i == 0 || n % (i + 2) == 0) return false;}
+        return true;
+    }
+};
+
+int main()
+{
+    // Example
+    // n =      10, 0, 1
+    // Output =  4, 0, 0
+    const std::vector<int> n = {10,0,1};
+    std::ranges::for_each(n, [](const int x)
+    {
+        std::cout<<"n: " <<x<<"; "<<"BruteforceCPP17: "<< Solution::count_primes_bruteforce_cpp17(x) << std::endl;
+        std::cout<<"n: " <<x<<"; "<<"BruteforceCPP20: "<< Solution::count_primes_bruteforce_cpp20(x) << std::endl;
+        std::cout << "---------" << std::endl;
+    });
+}
